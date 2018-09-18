2fa539480785cacfee8b067ca946b02a1fae5b88
Pierre Baseley <pierrebaseley@Pierres-MacBook-Pro.local>
2018-09-18 12:50:01 +0100

    Completed first lesson

---
+++ app-swoosh2/app-swoosh2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh2
+//
+//  Created by Pierre Baseley on 18/09/2018.
+//  Copyright © 2018 Pierre Baseley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
