abd2f4e829536558ca33a6509bad5637c84be7b7
Pierre Baseley <pierrebaseley@Pierres-MacBook-Pro.local>
2018-09-19 20:46:18 +0100

    Added part way through 2nd lesson then crashed

--- app-swoosh2/app-swoosh2/ViewController.swift
+++ app-swoosh2/app-swoosh2/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BGImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        swoosh.frame = CGRect (x: view.frame.size.width / 2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height) //view is screen, frame is its size and position, size specifies size and width is width of screen as a dimension so / 2 is midway
+        
+        BGImage.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
