9c4b8e2b65fe6fd5a640566137b15cee4d277fef
Pierre Baseley <pierrebaseley@Pierres-MacBook-Pro.local>
2018-10-01 12:53:51 +0100

    Completed Lesson 8

---
+++ app-swoosh2/app-swoosh2/Model/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  app-swoosh2
+//
+//  Created by Pierre Baseley on 01/10/2018.
+//  Copyright © 2018 Pierre Baseley. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+    
+}
